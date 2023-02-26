bd285341bf60c884235f51ed95b89cbd203592b6
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-02-26 21:50:08 +0600

    react pizza
    divided into components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/app.scss'
+import Header from "./components/header";
+import Categories from "./components/categories";
+import Sort from "./components/sort";
+import PizzaBlock from "./components/pizza-block";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+
+            <div className="wrapper">
+                <Header/>
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories/>
+                            <Sort/>
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                        <PizzaBlock title={"Чизбургер-пицца"} price ={500}/>
+                        <PizzaBlock title={"Мексиканская"} price ={400}/>
+                        <PizzaBlock title={"Пепперони"} price ={300}/>
+                        <PizzaBlock title={"Маргарита"} price ={100}/>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
